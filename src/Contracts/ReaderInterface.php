7b8cd0450c3818fab614080bbdf5102ce67845c5
Thiago Barcala <thiago.barcala@gmail.com>
2025-01-10 09:43:05 +0100

    Remove templates from contracts

--- src/Contracts/ReaderInterface.php
+++ src/Contracts/ReaderInterface.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace Auxmoney\Avro\Contracts;
 
-/**
- * @template T
- */
 interface ReaderInterface
 {
-    /**
-     * @return T
-     */
     public function read(ReadableStreamInterface $stream): mixed;
 }
--- src/Contracts/WriterInterface.php
+++ src/Contracts/WriterInterface.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace Auxmoney\Avro\Contracts;
 
-/**
- * @template T
- */
 interface WriterInterface
 {
-    /**
-     * @param T $data
-     */
     public function write(mixed $data, WritableStreamInterface $stream): void;
 }
