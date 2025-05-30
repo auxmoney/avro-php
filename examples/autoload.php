5caf3c486104963bc5b24dcda689fe5eae69ce44
Thiago Barcala <thiago.barcala@gmail.com>
2025-05-30 10:45:20 +0200

    Remove apache code

---
+++ examples/autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
+
+if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../vendor/autoload.php';
+}
