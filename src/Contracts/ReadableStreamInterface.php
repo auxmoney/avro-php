2b0ebe7b6095f82e042fb2249c558595a776a2aa
Thiago Barcala <thiago.barcala@gmail.com>
2025-07-25 17:59:45 +0200

    Validate read operations

--- src/Contracts/ReadableStreamInterface.php
+++ src/Contracts/ReadableStreamInterface.php
@@ -4,9 +4,21 @@ declare(strict_types=1);
 
 namespace Auxmoney\Avro\Contracts;
 
+use Auxmoney\Avro\Exceptions\StreamReadException;
+
 interface ReadableStreamInterface
 {
+    /**
+     * This method must return the number of bytes requested or throw an exception if the stream is exhausted.
+     *
+     * @throws StreamReadException
+     */
     public function read(int $count): string;
 
+    /**
+     * This method must skip the number of bytes requested or throw an exception if the stream is exhausted.
+     *
+     * @throws StreamReadException
+     */
     public function skip(int $count): void;
 }
