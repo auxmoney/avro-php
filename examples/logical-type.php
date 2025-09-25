a4987251398d57e6a7d6b2d77b9a9fee26bbbc17
Thiago Barcala <thiago.barcala@gmail.com>
2025-09-25 18:24:49 +0200

    Update README.md

--- examples/logical-type.php
+++ examples/logical-type.php
@@ -3,57 +3,24 @@
 require_once __DIR__ . '/autoload.php';
 
 use Auxmoney\Avro\AvroFactory;
-use Auxmoney\Avro\Contracts\LogicalTypeFactoryInterface;
-use Auxmoney\Avro\Contracts\LogicalTypeInterface;
-use Auxmoney\Avro\Contracts\Options;
-use Auxmoney\Avro\Contracts\ValidationContextInterface;
+use Auxmoney\Avro\ValueObject\Decimal;
 
-class Base64ExampleType implements LogicalTypeInterface
-{
-    public function validate(mixed $datum, ?ValidationContextInterface $context): bool
-    {
-        if (!is_string($datum)) {
-            $context?->addError('expected string, got ' . gettype($datum));
-            return false;
-        }
+$avroFactory = AvroFactory::create();
 
-        return true;
-    }
+$schema = '{"type": "long", "logicalType": "timestamp-millis"}';
+$encodedData = "\x00";
 
-    public function denormalize(mixed $datum): mixed
-    {
-        return base64_decode($datum);
-    }
+$reader = $avroFactory->createReader($schema);
+$buffer = $avroFactory->createReadableStreamFromString($encodedData);
+$decodedData = $reader->read($buffer);
 
-    public function normalize(mixed $datum): mixed
-    {
-        return base64_encode($datum);
-    }
-}
+var_dump($decodedData);
 
-class Base64ExampleTypeFactory implements LogicalTypeFactoryInterface
-{
-    public function getName(): string
-    {
-        return 'base64-example';
-    }
 
-    public function create(array $attributes): Auxmoney\Avro\Contracts\LogicalTypeInterface
-    {
-        return new Base64ExampleType();
-    }
-}
-
-$options = new Options(logicalTypeFactories: [new Base64ExampleTypeFactory()]);
-$avroFactory = AvroFactory::create($options);
-
-$schema = '{"type": "string", "logicalType": "base64-example"}';
+$schema = '{"type": "bytes", "logicalType": "decimal", "precision": 10, "scale": 4}';
+$decodedData = Decimal::fromString('3.14159');
 
 $writer = $avroFactory->createWriter($schema);
-$writeBuffer = $avroFactory->createStringBuffer();
-$writer->write('Hello, World!', $writeBuffer);
-var_dump($writeBuffer->__toString());
-
-$readBuffer = $avroFactory->createReadableStreamFromString("(SGVsbG8sIFdvcmxkIQ==");
-$reader = $avroFactory->createReader($schema);
-var_dump($reader->read($readBuffer));
+$buffer = $avroFactory->createStringBuffer($decodedData);
+$writer->write($decodedData, $buffer);
+var_dump(bin2hex($buffer->__toString()));
